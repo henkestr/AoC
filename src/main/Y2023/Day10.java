b1f74aad38e7bebc705bb6b68b24f3e5b2039269
Henrik Strand <henrik.strand@sogeti.com>
2023-12-14 13:00:55 +0100

    Added day 10

--- src/main/Y2023/Day10.java
+++ src/main/Y2023/Day10.java
@@ -2,17 +2,201 @@ package main.Y2023;
 
 import main.IDay;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day10 implements IDay {
 
+    public static class Node {
+        public Map<Node, Integer> touchingNodes = new HashMap<>();
+        public char c;
+        int x;
+        int y;
+
+        public Node(char c, int x, int y) {
+            this.c = c;
+            this.x = x;
+            this.y = y;
+        }
+
+        public void process(Node[][] matrix) {
+            switch (c) {
+                case '|':
+                    putIfExist(getUp(matrix));
+                    putIfExist(getDown(matrix));
+                    break;
+                case '-':
+                    putIfExist(getLeft(matrix));
+                    putIfExist(getRight(matrix));
+                    break;
+                case 'L':
+                    putIfExist(getRight(matrix));
+                    putIfExist(getUp(matrix));
+                    break;
+                case 'J':
+                    putIfExist(getLeft(matrix));
+                    putIfExist(getUp(matrix));
+                    break;
+                case '7':
+                    putIfExist(getLeft(matrix));
+                    putIfExist(getDown(matrix));
+                    break;
+                case 'F':
+                    putIfExist(getDown(matrix));
+                    putIfExist(getRight(matrix));
+                    break;
+                case 'S', '.':
+                    break;
+            }
+        }
+
+        private void putIfExist(Node node) {
+            if (node != null) {
+                touchingNodes.put(node, 1);
+            }
+        }
+
+        public Node getUp(Node[][] matrix) {
+            return y > 0 ? matrix[x][y - 1] : null;
+        }
+
+        public Node getDown(Node[][] matrix) {
+            return y < matrix[0].length - 1 ? matrix[x][y + 1] : null;
+        }
+
+        public Node getLeft(Node[][] matrix) {
+            return x > 0 ? matrix[x - 1][y] : null;
+        }
+
+        public Node getRight(Node[][] matrix) {
+            return x < matrix.length - 1 ? matrix[x + 1][y] : null;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "c=" + c +
+                    ", x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+    }
+
     @Override
     public String solvePart1(List<String> input) {
-        return String.valueOf(0);
+        Node[][] matrix = new Node[input.getFirst().length()][input.size()];
+        List<Node> allNodes = new ArrayList<>();
+        Node startNode = null;
+
+        for (int y = 0; y < input.size(); y++) {
+            var chars = input.get(y).toCharArray();
+            for (int x = 0; x < chars.length; x++) {
+                char currChar = chars[x];
+
+                var newNode = new Node(currChar, x, y);
+                matrix[x][y] = newNode;
+                allNodes.add(newNode);
+
+                if (currChar == 'S') {
+                    startNode = newNode;
+                }
+            }
+        }
+
+        allNodes.forEach(node -> node.process(matrix));
+        allNodes.forEach(node -> {
+            for (Node other : node.touchingNodes.keySet()) {
+                if (other.c == 'S') {
+                    other.touchingNodes.put(node, 1);
+                }
+            }
+        });
+
+        var distance = sortBoundaryList(startNode).size() / 2;
+
+        return String.valueOf(distance);
     }
 
     @Override
     public String solvePart2(List<String> input) {
-        return String.valueOf(0);
+        Node[][] matrix = new Node[input.getFirst().length()][input.size()];
+        List<Node> allNodes = new ArrayList<>();
+        Node startNode = null;
+
+        for (int y = 0; y < input.size(); y++) {
+            var chars = input.get(y).toCharArray();
+            for (int x = 0; x < chars.length; x++) {
+                char currChar = chars[x];
+
+                var newNode = new Node(currChar, x, y);
+                matrix[x][y] = newNode;
+                allNodes.add(newNode);
+
+                if (currChar == 'S') {
+                    startNode = newNode;
+                }
+            }
+        }
+        allNodes.forEach(node -> node.process(matrix));
+        allNodes.forEach(node -> {
+            for (Node other : node.touchingNodes.keySet()) {
+                if (other.c == 'S') {
+                    other.touchingNodes.put(node, 1);
+                }
+            }
+        });
+
+        var boundaries = sortBoundaryList(startNode);
+
+        var area = CalcArea(boundaries);
+
+        //Picks theorem tells us that A = i + (b/2) - 1
+        //where A = area, i = number of internal points, b = number of boundary points
+
+        // A - (b/2) + 1  = i
+        var internalPoints = area - boundaries.size() / 2 + 1;
+
+        // Picks theorem & Shoelace
+        return String.valueOf(internalPoints);
+    }
+
+    private List<Node> sortBoundaryList(Node start) {
+        var res = new ArrayList<Node>();
+        res.add(start);
+        var next = start.touchingNodes
+                .keySet()
+                .stream()
+                .findFirst()
+                .orElse(null);
+
+        while (next != null) {
+            res.add(next);
+            next = next.touchingNodes
+                    .keySet()
+                    .stream()
+                    .filter(n -> !res.contains(n))
+                    .findFirst()
+                    .orElse(null);
+        }
+        return res;
+    }
+
+    // Get area using shoelace formula
+    private long CalcArea(List<Node> border) {
+        var area = 0L;
+
+        // Calculate value of Shoelace formula
+        int j = border.size() - 1;
+        for (int i = 0; i < border.size(); i++) {
+            area += (long) (border.get(j).x + border.get(i).x) * (border.get(j).y - border.get(i).y);
+
+            // j is previous vertex to i
+            j = i;
+        }
+
+        // Return absolute value
+        return Math.abs(area / 2);
     }
 }
--- test/Y2023/Day10Test.java
+++ test/Y2023/Day10Test.java
@@ -10,17 +10,71 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day10Test {
     private static final Day10 DAY = new Day10();
-    private static final List<String> PART_1_AND_2_INPUT = """
+    private static final List<String> PART_1_1_INPUT = """
+            .....
+            .S-7.
+            .|.|.
+            .L-J.
+            .....
             """.lines().toList();
 
+    private static final List<String> PART_1_2_INPUT = """
+            ..F7.
+            .FJ|.
+            SJ.L7
+            |F--J
+            LJ...
+            """.lines().toList();
+
+    private static final List<String> PART_2_1_INPUT = """
+            ...........
+            .S-------7.
+            .|F-----7|.
+            .||.....||.
+            .||.....||.
+            .|L-7.F-J|.
+            .|..|.|..|.
+            .L--J.L--J.
+            ...........
+            """.lines().toList();
+
+    private static final List<String> PART_2_2_INPUT = """
+            .F----7F7F7F7F-7....
+            .|F--7||||||||FJ....
+            .||.FJ||||||||L7....
+            FJL7L7LJLJ||LJ.L-7..
+            L--J.L7...LJS7F-7L7.
+            ....F-J..F7FJ|L7L7L7
+            ....L7.F7||L7|.L7L7|
+            .....|FJLJ|FJ|F7|.LJ
+            ....FJL-7.||.||||...
+            ....L---J.LJ.LJLJ...
+            """.lines().toList();
+
+    private static final List<String> PART_2_3_INPUT = """
+            FF7FSF7F7F7F7F7F---7
+            L|LJ||||||||||||F--J
+            FL-7LJLJ||||||LJL-77
+            F--JF--7||LJLJ7F7FJ-
+            L---JF-JLJ.||-FJLJJ7
+            |F|F-JF---7F7-L7L|7|
+            |FFJF7L7F-JF7|JL---7
+            7-L-JL7||F7|L7F-7F7|
+            L.L7LFJ|||||FJL7||LJ
+            L7JLJL-JLJLJL--JLJ.L
+            """.lines().toList();
 
     @Test
     public void part1() {
-        assertEquals("0", DAY.solvePart1(PART_1_AND_2_INPUT));
+        assertEquals("4", DAY.solvePart1(PART_1_1_INPUT));
+        assertEquals("8", DAY.solvePart1(PART_1_2_INPUT));
+
     }
 
     @Test
     public void part2() {
-        assertEquals("0", DAY.solvePart2(PART_1_AND_2_INPUT));
+        assertEquals("4", DAY.solvePart2(PART_2_1_INPUT));
+        assertEquals("8", DAY.solvePart2(PART_2_2_INPUT));
+        assertEquals("10", DAY.solvePart2(PART_2_3_INPUT));
     }
 }
